23f34bf46b21b7742e961695774abec533d270a1
Albert Lam <albert233@hotmail.com>
2018-03-11 19:46:12 +1100

    Added calculate.java

---
+++ Algorithms, Sedgewick & Wayne/calculate.java
@@ -0,0 +1,9 @@
+public class calculate
+{
+    public static void main(String[] args)
+    {
+        StdOut.println((0 + 15) / 2);
+        StdOut.println(2.0e-6 * 100000000.1);
+        StdOut.println(true && false || true && true);
+    }
+}
