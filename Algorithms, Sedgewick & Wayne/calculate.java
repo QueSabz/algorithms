1fd09ee60939d60c39d18ef81eab930f68d38abe
Albert Lam <albert233@hotmail.com>
2018-03-13 20:58:55 +1100

    Updated calculate.java

--- Algorithms, Sedgewick & Wayne/calculate.java
+++ Algorithms, Sedgewick & Wayne/calculate.java
@@ -5,5 +5,13 @@ public class calculate
         StdOut.println((0 + 15) / 2);
         StdOut.println(2.0e-6 * 100000000.1);
         StdOut.println(true && false || true && true);
+        double a = (1 + 2.236) / 2;
+        double b = 1 + 2 + 3 + 4.0;
+        boolean c = 4.1 >= 4;
+        String d = 1 + 2 + "3";
+        StdOut.println(a + " is of type: " + ((Object)a).getClass().getName());
+        StdOut.println(b + " is of type: " + ((Object)b).getClass().getName());
+        StdOut.println(c + " is of type: " + ((Object)c).getClass().getName());
+        StdOut.println(d + " is of type: " + ((Object)d).getClass().getName());
     }
 }
