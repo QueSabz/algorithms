d93d5a586f8a8e2600cda56a2e0fe385a263e1ea
Albert Lam <albert233@hotmail.com>
2018-03-30 18:49:23 +1100

    Updated logfactorial.java

--- Algorithms, Sedgewick & Wayne/logfactorial.java
+++ Algorithms, Sedgewick & Wayne/logfactorial.java
@@ -2,15 +2,15 @@ import java.lang.Math;
 
 public class logfactorial
 {
-    public static double logfactorial(int N)
+    public static double factorial(int N)
     {
         if (N <= 1) return 1;
-        else return N * logfactorial(N-1);
+        else return N * factorial(N-1);
     }
 
     public static void main(String[] args)
     {
         int N = Integer.parseInt(args[0]);
-        StdOut.println(Math.log(logfactorial(N)));
+        StdOut.println(Math.log(factorial(N)));
     }
 }
